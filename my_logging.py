13af97bf34f4aa0ac83eb547a52a3ae89309219e
Andrew J. Sender <AndrewJ.Sender@gmail.com>
2026-08-27 11:32:05 -0400

    license

--- my_logging.py
+++ my_logging.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2026 Andrew J. Sender <AndrewJ.Sender@gmail.com>
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause License found in
+# the LICENSE file in the root directory of this source tree.
 
 import logging
 
